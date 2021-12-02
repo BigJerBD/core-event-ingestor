770d255dd727ac9e61df3527d7c8f3b46db20385
Jeremie Bigras-Dunberry <bigjerbd@gmail.com>
2021-12-01 22:56:47 -0500

    update: add struct deserialization

--- src/services/todoist.rs
+++ src/services/todoist.rs
@@ -1,7 +1,27 @@
+use actix_web::web::Json;
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use serde::{Deserialize, Serialize};
+use serde_json;
+
+#[derive(Serialize, Deserialize)]
+struct TodoistEvent {
+    user_id: u64,
+    version: String,
+    initiator: serde_json::Value,
+    event_name: String,
+    event_data: serde_json::Map<String, serde_json::Value>,
+}
 
 //#[api_v2_operation]
 #[post("/todoist/webhook")]
-pub async fn webhook(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
+pub async fn webhook(event: web::Json<TodoistEvent>) -> impl Responder {
+    //todo validate HMAC
+    let project_id = event
+        .event_data
+        .get("project_id")
+        .unwrap()
+        .as_u64()
+        .unwrap();
+
+    HttpResponse::Ok()
 }
